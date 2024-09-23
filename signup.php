7c1900c26889817d680e6a45dd792a8e66457456
Earl Novesteras <ejdnovesteras@usm.edu.ph>
2024-09-23 20:51:14 +0800

    addition

--- signup.php
+++ signup.php
@@ -28,7 +28,7 @@
 
                 <form id="login">
                     <div class="input-group mb-3">
-                        <input type="text" class="form-control" placeholder="echo "<td>" . htmlspecialchars($obj["studentno"]) . "</td>";" id="fullname" name="fullname">
+                        <input type="text" class="form-control" placeholder="fullname" id="fullname" name="fullname">
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-user-tie"></span>
